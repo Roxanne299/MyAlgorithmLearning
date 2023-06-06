6d800e89f079ff76d1d198af4dc3d7792a663ccd
waar <1727380670@qq.com>
2023-06-06 15:50:17 +0800

    add note

--- Algorithm/src/com/zgy/Main.java
+++ Algorithm/src/com/zgy/Main.java
@@ -5,32 +5,16 @@ import java.io.*;
 import java.util.*;
 
 class Main{
-    public static int N = 55;
-    public static int[][][] dp = new int[2*N][N][N];
-    public static int[][] g = new int[N][N];
-    public static Scanner sc = new Scanner(System.in);
+    public static int N = 3010;
+    public static char[] a = new char[N],b = new char[N];
+    public static int[][] dp = new int[N][N];
+    public static BufferedReader br = new BufferedReader(new InputSteamReader(System.in));
 
-    public static void main(String[] args){
-        int m = sc.nextInt(),n = sc.nextInt();
-        for(int i = 1;i <= m;i++)
-            for(int j = 1;j <= n;j++)
-                g[i][j] = sc.nextInt();
+    public static void main(String[] args)throws Exception{
+        int n = Integer.parseInt(br.readLine());
+        a = br.readLine().toCharArray();
+        b = br.readLine().toCharArray();
+        
 
-        for(int k = 2;k <= m + n;k++)
-            for(int i1 = 1;i1 <= m;i1++)
-                for(int i2 = 1;i2 <= m;i2++)
-                {
-                    int j1 = k - i1,j2 = k - i2;
-                    if(j1 >= 1 && j2 >= 1 && j1 <= n && j2 <= n){
-                        int t = g[i1][j1];
-                        if(i1 != i2) t += g[i2][j2];
-                        dp[k][i1][i2] = Math.max(dp[k-1][i1][i2] + t,dp[k][i1][i2]);
-                        dp[k][i1][i2] = Math.max(dp[k-1][i1-1][i2] + t,dp[k][i1][i2]);
-                        dp[k][i1][i2] = Math.max(dp[k-1][i1][i2-1] + t,dp[k][i1][i2]);
-                        dp[k][i1][i2] = Math.max(dp[k-1][i1-1][i2-1] + t,dp[k][i1][i2]);
-                    }
-                }
-
-        System.out.print(dp[n+m][m][n]);
     }
 }
