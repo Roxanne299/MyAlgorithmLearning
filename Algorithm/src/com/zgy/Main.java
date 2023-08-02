19657161bf70b087c64206e64f870f59825a661a
waar <1727380670@qq.com>
2023-08-02 16:43:29 +0800

    add notes

--- Algorithm/src/com/zgy/Main.java
+++ Algorithm/src/com/zgy/Main.java
@@ -8,12 +8,9 @@ class Main{
     public static int N = 3010;
     public static char[] a = new char[N],b = new char[N];
     public static int[][] dp = new int[N][N];
-    public static BufferedReader br = new BufferedReader(new InputSteamReader(System.in));
 
     public static void main(String[] args)throws Exception{
-        int n = Integer.parseInt(br.readLine());
-        a = br.readLine().toCharArray();
-        b = br.readLine().toCharArray();
+
 
 
     }
