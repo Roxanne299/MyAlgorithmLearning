3d6c528f9e7546bc529b50f57f9b00a72bc081f3
waar <1727380670@qq.com>
2023-10-13 15:01:30 +0800

    添加了线段树模板以及更新了刷题笔记

--- Algorithm/src/com/zgy/Main.java
+++ Algorithm/src/com/zgy/Main.java
@@ -1,35 +1,9 @@
 package com.zgy;
 
-
 import java.io.*;
 import java.util.*;
 
 class Main{
-    public static int N = 500010;
-    public static long res = 0;
-    public static int[] a = new int[N],temp = new int[N];
-    public static Scanner sc = new Scanner(System.in);
 
 
-    public static void main(String[] args){
-        String s = sc.next();
-        int mn = 0,ln = 0,sn = 0,l = 0,m = 0;
-        for(int i = 1;i <= s.length();i++){
-            char temp = s.charAt(i-1);
-            if(temp == 'L') ln++;
-            if(temp == 'S') sn++;
-            if(temp == 'M') mn++;
-        }
-
-        for(int i = 1;i <= s.length();i++){
-            if(i <= ln && s.charAt(i) != 'L') l ++;
-            if(i > ln && i <= ln + mn && s.charAt(i) != 'M') m++;
-        }
-        int res = 0;
-        for(int i = ln+1;i <= ln + mn;i++){
-            if(s.charAt(i) == 'L') res --;
-        }
-        System.out.print(l + res + m);
-
-    }
 }
---
+++ Algorithm/src/com/zgy/learn/algorithm/data_structure/SegmentTree.java
@@ -0,0 +1,58 @@
+package com.zgy.learn.algorithm.data_structure;
+
+//线段树模板
+public class SegmentTree {
+    public static int N = 200010;
+    public static Node[] node = new Node[4*N];
+
+    public static void pushup(int u){
+        node[u].max = Math.max(node[u<<1].max,node[u<<1|1].max);
+    }
+
+    public static void build(int u,int l,int r){
+        node[u] = new Node(l,r,0);
+        if(node[u].l == node[u].r) return;
+        int mid = (l + r) >> 1;
+        build(u<<1,l,mid);
+        build(u<<1|1,mid+1,r);
+        pushup(u);
+    }
+
+    public static int query(int u,int l,int r){
+        int mid = (node[u].l + node[u].r) >> 1,v = 0;
+        if(node[u].l >= l && node[u].r <= r) return node[u].max;
+        if(l <= mid) v = query(u<<1,l,r);
+        if(r > mid) v = Math.max(v,query(u<<1|1,l,r));
+        return v;
+    }
+    /*query 不是这么写的
+        public static int query(int u,int l,int r){
+        int mid = (l + r) >> 1,v = 0;
+        if(node[u].l >= l && node[u].r <= r) return node[u].max;
+        if(node[u].l <= mid) v = query(u<<1,l,r);
+        if(node[u].r > mid) v = Math.max(v,query(u<<1|1,l,r));
+        return v;
+    }*/
+
+    public static void modify(int u,int x,int v){
+        if(node[u].l == x && node[u].r == x) node[u].max = v;
+        else{
+            int mid = (node[u].l + node[u].r) >> 1;
+            if(x <= mid) modify(u<<1,x,v);
+            else modify(u<<1|1,x,v);
+            pushup(u);
+        }
+    }
+
+    static class Node{
+        int l;
+        int r;
+        int max;
+        public Node(int l,int r,int max){
+            this.l = l;
+            this.r = r;
+            this.max = max;
+        }
+    }
+
+}
