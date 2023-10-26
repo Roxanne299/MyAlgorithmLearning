1bef587094d3f58e2ebaa43adb9c5eea6e25206e
waar <1727380670@qq.com>
2023-10-26 17:02:26 +0800

    更新背包笔记

--- Algorithm/src/com/zgy/Main.java
+++ Algorithm/src/com/zgy/Main.java
@@ -1,110 +1,51 @@
 package com.zgy;
 
-import com.sun.javafx.image.IntPixelGetter;
-
 import java.io.*;
 import java.util.*;
 
 class Main{
-
-    public static int N = 100010,P;
-    public static Node[] tr = new Node[4*N];
-    public static int[] a = new int[N];
+    public static int N = 110,V = 110;
+    public static int[] v = new int[N];
+    public static int[][] dp = new int[N][V],s = new int[N][V];
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void pushup(int u){
-        tr[u].sum = (int)((long)tr[u<<1].sum + tr[u<<1|1].sum % P);
-    }
-
-    public static void build(int u,int l,int r){
-        tr[u] = new Node(l,r,0,0,1);
-        if(l == r){
-            tr[u].sum = a[l];
-        }else{
-            int mid = l + r >> 1;
-            build(u<<1,l,mid);
-            build(u<<1|1,mid+1,r);
-            pushup(u);
-        }
-    }
-
-
-    public static void updateNode(Node u,int mul,int add){
-        u.sum = (int)((long)mul * u.sum % P + (long)add * (u.r - u.l + 1) % P) % P;
-        u.add = (int)((long)u.add * mul + add % P);
-        u.mul = (int)((long)u.mul * mul % P);
-    }
-    public static void pushdown(int u){
-        updateNode(tr[u<<1],tr[u].mul,tr[u].add);
-        updateNode(tr[u<<1|1],tr[u].mul,tr[u].add);
-        tr[u].add = 0;
-        tr[u].mul = 1;
-    }
-    public static void modify(int u,int l,int r,int mul,int add){
-        if(tr[u].l >= l && tr[u].r <= r) {
-            updateNode(tr[u],mul,add);
-        }else{
-            pushdown(u);
-            int mid = tr[u].l + tr[u].r >> 1;
-            if(l <= mid) modify(u<<1,l,r,mul,add);
-            if(r > mid) modify(u<<1|1,l,r,mul,add);
-            pushup(u);
-        }
-    }
-
-
-    public static int query(int u,int l,int r){
-        if(tr[u].l >= l && tr[u].r <= r) return tr[u].sum;
-        else{
-            pushdown(u);
-            int mid = tr[u].l + tr[u].r >> 1;
-            int res = 0;
-            if(l <= mid) res = (int)((long)res + query(u<<1,l,r) % P);
-            if(r > mid) res = (int)((long)res + query(u<<1|1,l,r) % P);
-            return res;
-        }
-    }
-
-    public static void main(String[] args)throws Exception{
+    public static void main(String[] args)throws Exception {
         String[] s1 = br.readLine().split(" ");
-        int n = Integer.parseInt(s1[0]);
-        P = Integer.parseInt(s1[1]);
-        String[] s2 = br.readLine().split(" ");
-        for(int i = 1;i <= n;i++) a[i] = Integer.parseInt(s2[i-1]);
+        int n = Integer.parseInt(s1[0]),v = Integer.parseInt(s1[1]);
+        int k1 = Integer.parseInt(br.readLine());
+        for(int i = 1;i <= n;i++){
+            String[] s2 = br.readLine().split(" ");
+            for(int j = 1;j <= v;j++){
+                s[i][j] = Integer.parseInt(s2[j-1]);
+            }
 
-        int m = Integer.parseInt(br.readLine());
-        build(1,1,n);
+        }
+        for(int i = 1;i <= n;i++) {
+            for(int j = 1;j <= v;j++)
+            {
+                dp[i][j] = dp[i-1][j];
+                for(int k = 1;k <= v;k++)
+                    if(j >= k) dp[i][j] = Math.max(dp[i][j],dp[i-1][j-k]+s[i][k]);
+            }
 
-        while((m--)!=0){
-            String[] s3 = br.readLine().split(" ");
-            int op = Integer.parseInt(s3[0]),l = Integer.parseInt(s3[1]),r =Integer.parseInt(s3[2]);
-            if(op == 1){
-                int d = Integer.parseInt(s3[3]);
-                modify(1,l,r,d,0);
-            }else if(op == 2){
-                int d = Integer.parseInt(s3[3]);
-                modify(1,l,r,1,d);
-            }else{
-                System.out.println(query(1,l,r));
+        }
+
+        bw.write(String.valueOf(dp[n][v]));
+
+        for(int i = n,j = v;i >= 1;i--){
+            for(int k = 1;k <= v;k++){
+                if(j >= k && dp[i][j] == dp[i-1][j - k] + s[i][k]){
+                    bw.write(String.valueOf(i)+" "+String.valueOf(k)+"\n");
+                    j-=k;
+                }
             }
         }
 
-     }
 
-     static class Node{
-        int l;
-        int r;
-        int sum;
-        int add;//懒标记
-        int mul;//懒标记
 
-         public Node(int l, int r, int sum, int add, int mul) {
-             this.l = l;
-             this.r = r;
-             this.sum = sum;
-             this.add = add;
-             this.mul = mul;
-         }
-     }
+
+        bw.flush();
+
+    }
 }
---
+++ Algorithm/src/com/zgy/learn/NewThread.java
@@ -0,0 +1,16 @@
+package com.zgy.learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewThread{
+    protected NewThread(){
+
+    }
+
+    @Override
+    public String toString() {
+        System.out.println();
+        return "";
+    }
+}
