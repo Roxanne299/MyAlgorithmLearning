c5f48d7f895bfb05f06fb367937cd79321cd4a14
waar <1727380670@qq.com>
2023-11-01 15:55:51 +0800

    更新状态机

--- Algorithm/src/com/zgy/Main.java
+++ Algorithm/src/com/zgy/Main.java
@@ -3,49 +3,33 @@ package com.zgy;
 import java.io.*;
 import java.util.*;
 
+import java.io.*;
+import java.util.*;
+
 class Main{
-    public static int N = 110,V = 110;
-    public static int[] v = new int[N];
-    public static int[][] dp = new int[N][V],s = new int[N][V];
+    public static int N = 100010,J = 110;
+    public static int[] a = new int[N];
+    public static int[][] dp = new int[N][3];//表示前N天手中有货，前N天手中无货第一天，前N天手中无货大于1天
     public static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    public static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-    public static void main(String[] args)throws Exception {
+    public static void main(String[] args)throws Exception{
         String[] s1 = br.readLine().split(" ");
-        int n = Integer.parseInt(s1[0]),v = Integer.parseInt(s1[1]);
-        int k1 = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(s1[0]),j = Integer.parseInt(s1[1]);
+        String[] s2 = br.readLine().split(" ");
+        for(int i = 1;i <= n;i++) a[i] = Integer.parseInt(s2[i-1]);
+        for(int i = 0;i <= n;i++){
+            dp[i][1] = -0x3f3f3f3f;
+        }
         for(int i = 1;i <= n;i++){
-            String[] s2 = br.readLine().split(" ");
-            for(int j = 1;j <= v;j++){
-                s[i][j] = Integer.parseInt(s2[j-1]);
-            }
-
-        }
-        for(int i = 1;i <= n;i++) {
-            for(int j = 1;j <= v;j++)
-            {
-                dp[i][j] = dp[i-1][j];
-                for(int k = 1;k <= v;k++)
-                    if(j >= k) dp[i][j] = Math.max(dp[i][j],dp[i-1][j-k]+s[i][k]);
-            }
-
-        }
-
-        bw.write(String.valueOf(dp[n][v]));
-
-        for(int i = n,j = v;i >= 1;i--){
-            for(int k = 1;k <= v;k++){
-                if(j >= k && dp[i][j] == dp[i-1][j - k] + s[i][k]){
-                    bw.write(String.valueOf(i)+" "+String.valueOf(k)+"\n");
-                    j-=k;
-                }
-            }
+            //手中无货第一天
+            dp[i][0] = dp[i-1][2] + a[i];
+            //手中无货大于一天
+            dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]);
+            //手中有货
+            dp[i][2] = Math.max(dp[i-1][1] + a[i],dp[i-1][2]);
         }
 
 
-
-
-        bw.flush();
-
+        System.out.println(Math.max(dp[n][0],dp[n][1]));
     }
 }
